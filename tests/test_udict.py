b6fc7291e922a47f061c2d7fdbe6fbbfe0c1cae9
bleudev <spdd5724@gmail.com>
2024-05-25 13:57:07 +0300

    Add tests

---
+++ tests/test_udict.py
@@ -0,0 +1,79 @@
+import unittest
+
+from ufpy import UDict
+
+
+class UDictTestCase(unittest.TestCase):
+    def test_init(self):
+        d = UDict(hello=1, hi=2, default=10)
+        d2 = UDict({'hello': 1, 'hi': 2})
+        self.assertEqual(d.default, 10)
+        self.assertDictEqual(d.dictionary, d2.dictionary)
+        self.assertEqual(d, d2)
+
+    def test_keys_values_items(self):
+        d = UDict(hello=1, hi=2)
+
+        self.assertEqual(d.keys, list(d.dictionary.keys()))
+        self.assertEqual(d.values, list(d.dictionary.values()))
+        self.assertEqual(d.items, list(d.dictionary.items()))
+
+    def test_call(self):
+        d = {'hello': 1, 'hi': 2}
+        ud = UDict(d)
+
+        d2 = d
+
+        for k, v in d2.items():
+            d2[k] = v * 2
+
+        ud2 = ud(lambda k, v: v * 2)
+
+        self.assertDictEqual(d2, ud2.dictionary)
+
+    def test_reverse(self):
+        d = {'hello': 1, 'hi': 2}
+        ud = UDict(d)
+
+        keys, values = list(d.keys())[::-1], list(d.values())[::-1]
+        d2 = dict(list(zip(keys, values)))
+
+        self.assertDictEqual(d2, ud.reversed().dictionary)
+        self.assertEqual(reversed(ud), ud.reversed())
+        self.assertEqual(-ud, ud.reverse())
+
+    def test_sort(self):
+        d = {'hi': 2, 'hello': 1}
+        ud = UDict(d)
+        sd = {'hello': 1, 'hi': 2}
+        sud = UDict(sd)
+
+        self.assertDictEqual(ud.sorted().dictionary, sd)
+        self.assertEqual(sorted(ud), sud.items)
+        self.assertEqual(ud.sorted(), ud.sort())
+
+    def test_get_item(self):
+        d = UDict(hello=1, hi=2)
+
+        self.assertEqual(d['hi'], 2)
+        self.assertEqual(d[1], 1)
+        self.assertEqual(d[1:2], d.values)
+
+        self.assertEqual(d[-2:], d.values)
+
+        self.assertEqual(d[1], d['hello'])
+
+    def test_set_item(self):
+        d = UDict(hello=1, hi=2)
+        d['hi'] = 3
+        self.assertDictEqual(d.dictionary, {'hello': 1, 'hi': 3})
+
+        d[1] = 4
+        self.assertDictEqual(d.dictionary, {'hello': 4, 'hi': 3})
+
+        d[:] = [1, 2]
+        self.assertDictEqual(d.dictionary, {'hello': 1, 'hi': 2})
+
+
+if __name__ == '__main__':
+    unittest.main()
