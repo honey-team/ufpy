642345fb139056ebb002dc8d60a51a1f31e8db97
bleudev <spdd5724@gmail.com>
2024-06-05 02:16:38 +0300

    Many updates

--- ufpy/__init__.py
+++ ufpy/__init__.py
@@ -3,4 +3,5 @@ __version__ = '1.0-alpha'
 from .cmp import *
 from .math_op import *
 from .udict import *
+from .ustack import *
 from .utils import *
--- ufpy/ustack.py
+++ ufpy/ustack.py
@@ -1,50 +1,76 @@
 from __future__ import annotations
-from typing import Any, Generic, TypeVar
-from .math_op import r_generator
 
-VT = TypeVar("VT")
+from typing import Generic, TypeVar, Iterable
 
-__all__ =  (
+from .cmp import cmp_generator
+from .math_op import r_generator, i_generator
+
+T = TypeVar("T")
+T2 = TypeVar("T2")
+
+__all__ = (
     "UStack",
 )
 
+@cmp_generator
+@i_generator
 @r_generator
-class UStack(Generic[VT]):
-    def __init__(self, __list: list[VT] = []):
+class UStack(Generic[T]):
+    """
+    Class for simplifying working with stacks in Python.
+    """
+    def __init__(self, *elements: T, iterable: Iterable[T] = None):
+        if iterable:
+            elements = iterable
+        self.__list = list(elements) if elements else []
+
+    @property
+    def list(self) -> list[T]:
+        return self.__list
+
+    @list.setter
+    def list(self, value: list[T]):
+        self.__list = value
+
+    def pop(self) -> T:
         """
-        UStack's methods:
-            append(*items) - to add item to begin.
-            empty() - true if stack is empty.
-            pop() - delete the last element in stack.
+        Remove and return top element.
+
+        :return: Removed element
         """
-        self.__list = __list
-        
-    def pop(self) -> VT:
-        """Return and remove top element."""
         return self.__list.pop()
-    
-    def top(self) -> VT:
-        """Gets top element of stack."""
+
+    @property
+    def top(self) -> T | None:
+        """
+        Top element of stack.
+
+        :return: Top element
+        """
         if self:
             return self.__list[-1]
-        else:
-            return False
-        
-    def append(self, *items) -> UStack:
+        return None
+
+    def append(self, *items: T) -> UStack[T]:
         """Append items to stack"""
         self.__list.extend(items)
         return UStack(self.__list)
-    
-    def is_empty(self) -> bool:
-        return len(self) == 0
 
-    def __add__(self, stack: UStack[VT] | VT) -> UStack:
+    def __add__(self, stack: UStack[T2] | T2) -> UStack[T | T2]:
         if not isinstance(stack, UStack):
             stack = UStack(stack)
         return UStack(self.__list + stack.__list)
-    
+
+    # Booleans
     def __len__(self) -> int:
         return len(self.__list)
+
+    def is_empty(self) -> bool:
+        return len(self) == 0
     
     def __nonzero__(self) -> bool:
         return not self.is_empty()
+
+    # Transform to other types
+    def __repr__(self) -> str:
+        return f's{self.__list}'
