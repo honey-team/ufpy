185423d234cfb06bfd4537b05c37eea13e526019
bleudev <spdd5724@gmail.com>
2024-07-14 02:49:49 +0300

    Some fixes

--- ufpy/ulist.py
+++ ufpy/ulist.py
@@ -12,51 +12,45 @@ class UList(Generic[T]):
             __list = __list.listing
         self.__list = __list
 
-    def __neg__(self):
-        new_list = self.listing.copy()
-        new_list.reverse()
-        return UList(new_list)
-        
-
-    def __getitem__(self, key: int) -> UList[T]:
-        return self.listing[key]
-
-    def count(self, value) -> int:
-        return self.listing.count()
-    
-    def insert(self, index: SupportsIndex, object):
-        return UList(self.listing.insert(index, object))
-
-    def replace(self, old, new):
-        counter = 0
-        new_list = self.listing.copy()
-        for i in new_list:
-            if i == old:
-                new_list[counter] = new
-            counter += 1
-        self.listing = new_list
-
-    def copy(self):
-        return UList(self.listing.copy())
-
     @property
     def listing(self) -> list[T]:
-        return self.__list
-    
+        return self.__list.copy()
+
     @listing.setter
     def listing(self, value: AnyCollection[T] | set[T] | UList[T]):
         if isinstance(value, UList):
             value = value.listing
         self.__list = list(value)
 
+    def reversed(self):
+        new_list = self.listing
+        new_list.reverse()
+        return UList(new_list)
+
+    def count(self, value: T) -> int:
+        return self.__list.count(value)
+    
+    def insert(self, index: SupportsIndex, object: T) -> None:
+        self.__list.insert(index, object)
+
+    def replace(self, old, new):
+        new_list = self.listing
+        for i, v in enumerate(new_list):
+            if v == old:
+                new_list[i] = new
+        self.__list = new_list
+
     def __call__(self, func: Callable[[T], T]) -> UList[T]:
         new_dict = self.list
         for k, v in self:
             new_dict[k] = func(k, v)
         return UList(new_dict)
+
+    def __getitem__(self, key: int) -> T:
+        return self.__list[key]
     
     def __repr__(self) -> str:
-        return f'u{self.listing}'
+        return f'u{self.__list}'
 
     def __add__(self, other) -> UList[T]:
         new_list = self.listing.copy()
