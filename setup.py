3af6d6b4c9ef28660c4233a5118c10a7064b2b46
n0n1m <97027325+n0n1m@users.noreply.github.com>
2024-08-31 15:06:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,22 +22,6 @@ def derive_version() -> str: # this function is stolen from discord.py
     if not version:
         raise RuntimeError('version is not set')
 
-    if version.endswith(('a', 'b', 'rc')):
-        # append version identifier based on commit count
-        try:
-            import subprocess
-
-            p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = p.communicate()
-            if out:
-                version += out.decode('utf-8').strip()
-            p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = p.communicate()
-            if out:
-                version += '+g' + out.decode('utf-8').strip()
-        except Exception:
-            pass
-
     return version
 
 __version__ = derive_version()
