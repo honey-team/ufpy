632498b01c68619bd51fb348ed683da8874c8602
bleudev <aitiiigg1@gmail.com>
2025-02-04 10:44:16 +0300

    Write to stream / binary stream

--- ufpy/path/json_file_updater.py
+++ ufpy/path/json_file_updater.py
@@ -5,34 +5,60 @@ __all__ = (
 )
 
 import os.path
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, TypeVar, TextIO, overload
 from ujson import dumps, loads
+
+from ufpy import ReadWriteIO
 
 VT = TypeVar('VT')
 
 class JsonFileUpdater(Generic[VT]):
-    def __init__(self, json_file_path: str, indent: int = 4) -> None:
-        self.path = json_file_path
+    @overload
+    def __init__(self, path: str, indent: int = 4): ...
+    @overload
+    def __init__(self, stream: ReadWriteIO[str | bytes], indent: int = 4): ...
+    def __init__(self, stream_or_path: str | ReadWriteIO[str | bytes], indent: int = 4) -> None:
         self.indent = indent
         self.__d: dict[str, VT] | None = None
 
-        if not os.path.exists(self.path):
+        self.path = None
+        self.stream = None
+        if hasattr(stream_or_path, 'write') and hasattr(stream_or_path, 'read'):
+            self.stream = stream_or_path
+        else:
+            self.path = stream_or_path
+
+        if not (self.stream or os.path.exists(self.path)):
             print(f'Warning: No such file or directory: {self.path}. JsonFileUpdater will create it automaticly.')
             with open(self.path, encoding='utf-8', mode='x') as f:
                 f.write('{}')
     
     def __load(self) -> dict[str, VT]:
-        with open(self.path, encoding='utf-8') as f:
-            r = loads(f.read())
-        return r
+        if self.stream:
+            if not self.stream.read():
+                return {}
+            else:
+                d = self.stream.read()
+        else:
+            with open(self.path, encoding='utf-8') as f:
+                d = f.read()
+        if isinstance(d, bytes): d = d.decode('utf-8')
+        return loads(d)
     
     def __write(self, d: dict[str, VT]) -> None:
-        with open(self.path, 'w', encoding='utf-8') as f:
-            f.write(dumps(
-                d,
-                ensure_ascii=False,
-                indent=self.indent
-            ))
+        d = dumps(
+            d,
+            ensure_ascii=False,
+            indent=self.indent
+        )
+        if self.stream:
+            try:
+                self.stream.write(d)
+            except TypeError: # bytes-like object is required
+                self.stream.write(d.encode('utf-8'))
+        else:
+            with open(self.path, 'w', encoding='utf-8') as f:
+                f.write(d)
 
     def __get_dict(self, path: str, __dict: dict[str, Any]) -> dict[str, Any]:
         if not ' / ' in path:
