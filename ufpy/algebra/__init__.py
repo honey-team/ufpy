c975320ecaf8926924ec6e3b15790c1000496a35
bleudev <95937737+bleudev@users.noreply.github.com>
2025-03-21 11:58:03 +0300

    Sequence tests

--- ufpy/algebra/__init__.py
+++ ufpy/algebra/__init__.py
@@ -1,4 +1,2 @@
-from ufpy.algebra.arithmetic import *
+from ufpy.algebra.sequences import *
 from ufpy.algebra.fibonacci import *
-from ufpy.algebra.func_sequence import *
-from ufpy.algebra.geometric import *
--- ufpy/algebra/sequences.py
+++ ufpy/algebra/sequences.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 __all__ = (
     'FunctionSequence',
+    'ArithmeticProgression',
+    'GeometricProgression'
 )
 
-from typing import Callable, TypeVar, Generic, Literal, overload, Any
+from typing import Callable, TypeVar, Generic, overload, Any
+from abc import ABC, abstractmethod
 
 from ufpy.utils import mul
 from ufpy.cmp import cmp_generator
@@ -14,6 +17,7 @@ KT = TypeVar('KT', int, float, int | float)
 
 VT2 = TypeVar('VT2', int, float, int | float)
 KT2 = TypeVar('KT2', int, float, int | float)
+
 
 @cmp_generator
 class FunctionSequence(Generic[VT, KT]):
@@ -27,28 +31,16 @@ class FunctionSequence(Generic[VT, KT]):
             return int(x), value
         return None
 
-    def __resolve_sum_and_composition(self, kwarg: tuple[str, VT]) -> tuple[Literal['s', 'p'], int, VT] | None:
-        name, value = kwarg
-        name = name.lower()
-
-        if name.startswith('s') and (x := name.replace('s', '')).isdigit():
-            return 's', int(x), value
-        if name.startswith('p') and (y := name.replace('p', '')):
-            return 'p', int(y), value
-        return None
-
     def __process_float(self, x: float):
         return int(x) if x.is_integer() else x
-
-    def k_func(self, a_m: VT, a_n: VT, m: int, n: int) -> KT:
-        ...  # return k
 
     def __init_subclass__(cls, name_of_elements: str, **kwargs):
         super().__init_subclass__(**kwargs)
         cls.__name_of_elements = name_of_elements
 
-    def __init__(self, f: Callable[[int, KT, VT, int], VT], k: KT = None, **kwargs: VT):
+    def __init__(self, f: Callable[[int, KT, VT, int], VT], k_func: Callable[[VT, VT, int, int], KT], k: KT = None, **kwargs: VT):
         self.ref_func = f
+        self.__k_func = k_func
         self.f = lambda n: self.ref_func(n, self.k, self.first, 1)
 
         el = [x for i in kwargs.items() if (x := self.__resolve_item(i))]
@@ -61,15 +53,19 @@ class FunctionSequence(Generic[VT, KT]):
             return
         if len(el) >= 2:
             (i1, v1), (i2, v2) = el[0], el[1]
-            self.k = self.__process_float(self.k_func(v1, v2, i1, i2))
+            self.k = self.__process_float(self.__k_func(v1, v2, i1, i2))
             self.first = self.__process_float(self.ref_func(1, self.k, v1, i1))
             return
         raise Exception("__init__ can't get k")
 
     @overload
-    def __call__(self, n: int) -> VT: ...
+    def __call__(self, n: int) -> VT:
+        ...
+
     @overload
-    def __call__(self, start: int, end: int) -> list[VT]: ...
+    def __call__(self, start: int, end: int) -> list[VT]:
+        ...
+
     def __call__(self, start: int, end: int | None = None):
         start1 = start
         if not end:
@@ -77,7 +73,7 @@ class FunctionSequence(Generic[VT, KT]):
 
         r = []
 
-        for i in range(start1, end+1):
+        for i in range(start1, end + 1):
             r.append(self.__process_float(self.f(i)))
 
         return r if len(r) > 1 else r[0]
@@ -87,7 +83,7 @@ class FunctionSequence(Generic[VT, KT]):
             start, end = x if (x := n.start) else 1, n.stop
             return self(start, end)
         return self(n)
-    
+
     def __cmp__(self, other: FunctionSequence[VT2, KT2]):
         return self.k - other.k
 
@@ -97,19 +93,73 @@ class FunctionSequence(Generic[VT, KT]):
         return [l_or_v]
 
     @overload
-    def s(self, n: int) -> VT: ...
+    def s(self, n: int) -> VT:
+        """
+        Get sum of elements from `1` to `n`
+        """
+        ...
+
     @overload
-    def s(self, start: int, end: int) -> VT: ...
+    def s(self, start: int, end: int) -> VT:
+        """
+        Get sum of elements from `start` to `end`
+        """
+        ...
+
     def s(self, start: int, end: int | None = None) -> VT:
+        """
+        Get sum of elements
+        """
         if end is None:
             start, end = 1, start
         return sum(self.__check_for_list(self(start, end)))
 
     @overload
-    def p(self, n: int) -> VT: ...
+    def p(self, n: int) -> VT:
+        """
+        Get product of elements from `1` to `n`
+        """
+        ...
+
     @overload
-    def p(self, start: int, end: int) -> VT: ...
+    def p(self, start: int, end: int) -> VT:
+        """
+        Get product of elements from `start` to `end`
+        """
+        ...
+
     def p(self, start: int, end: int | None = None) -> VT:
+        """
+        Get product of elements
+        """
         if end is None:
             start, end = 1, start
         return mul(self.__check_for_list(self(start, end)))
+
+
+class ArithmeticProgression(FunctionSequence[VT, KT], name_of_elements='a'):
+
+    @property
+    def d(self) -> KT:
+        return self.k
+
+    def __init__(self, *, d: KT = None, **kwargs):
+        def f(n, k, am, m):
+            return am + k * (n - m)
+        def k_func(a_m: VT, a_n: VT, m: int, n: int) -> KT:
+            return (a_n - a_m) / (n - m)
+        super().__init__(f, k_func, d, **kwargs)
+
+class GeometricProgression(FunctionSequence[VT, KT], name_of_elements='b'):
+
+
+    @property
+    def q(self) -> KT:
+        return self.k
+
+    def __init__(self, *, q: KT = None, **kwargs):
+        def f(n, k, am, m):
+            return am * (k**(n-m))
+        def k_func(a_m: VT, a_n: VT, m: int, n: int) -> KT:
+            return (a_n / a_m) ** (1 / (n - m))
+        super().__init__(f, k_func, q, **kwargs)
